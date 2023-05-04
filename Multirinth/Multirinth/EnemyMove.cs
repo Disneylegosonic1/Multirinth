39b9b064ee1decc7f082147011263ead29573e75
Stumblingbee <98423114+Stumblingbee@users.noreply.github.com>
2023-05-04 11:44:29 -0500

    Add files via upload

--- Multirinth/Multirinth/EnemyMove.cs
+++ Multirinth/Multirinth/EnemyMove.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace EnemyMovement
 {
     internal class EnemyMove
     {
+        private Texture2D enemy;
+
+        Rectangle pos;
+        Boolean dir;
+        Boolean once;
+        public EnemyMove(Rectangle pos, Texture2D enemy)
+        {
+            this.pos = pos;
+            dir = false;
+            once = true;
+            this.enemy = enemy;
+        }
+
+        public void Update(GameTime gametime)
+        {
+            
+            if (once)
+            {
+                pos.X += 3;
+                once = false;
+            }
+            if (gametime.ElapsedGameTime.TotalMilliseconds % 500 == 0 && dir)
+            {
+                pos.X += 6;
+            }
+            else if(gametime.ElapsedGameTime.TotalMilliseconds % 500 != 0 && !dir)
+            {
+                pos.X -= 6;
+            }
+            
+        }
+
+        public void Draw(SpriteBatch _spritebatch, GameTime gametime)
+        { 
+            _spritebatch.Begin();
+            _spritebatch.Draw(enemy, pos, Color.White);
+            _spritebatch.End();
+        }
+
+
     }
 }
